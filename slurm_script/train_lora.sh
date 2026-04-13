243c033522e3a58f0cb36e0123053b9de181791d
liys-0 <liys.85@gmail.com>
2026-04-13 17:40:29 +0800

    update slurm file for train local lora

--- slurm_script/train_lora.sh
+++ slurm_script/train_lora.sh
@@ -23,6 +23,12 @@ source venv/bin/activate
 # need to set up the data path for the training script 
 # need to set up the latest version of the trained model using normal pfib data 
 # Run the LoRA training script
-python train_lora_normal_pfib.py --mask_dir /home/lys/projects/POC_Dadaset/20251208/patches/gt
+python train_lora_normal_pfib.py \
+    --mask_dir "/home/lys/projects/POC_Dadaset/20251208/patches/gt" \
+    --lora_save_dir "./output_lora" \
+    --batch_size 4 \
+    --learning_rate 1e-4 \
+    --max_epochs 100 \
+    --lora_rank 64
 
 echo "Job finished at $(date)"
